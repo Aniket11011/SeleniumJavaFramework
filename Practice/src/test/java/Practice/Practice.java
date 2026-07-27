7b19f9979f4760b6ba2d4d95846e89a563131dd4
aniket <aniketsalunkhe1004@gmail.com>
2026-07-27 16:16:29 +0530

    Initial Selenium Framework

---
+++ Practice/src/test/java/Practice/Practice.java
@@ -0,0 +1,40 @@
+package Practice;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Practice {
+	
+	public class testingtestng {
+
+		@BeforeClass
+		public void login() {
+			
+			
+			System.out.println("User logged in");
+		}
+		
+		
+		@Test(priority=2)
+		public void search() {
+			
+			System.out.println("User searched");
+		}
+		
+		
+		@Test(priority=1)
+		public void Advsearch() {
+			
+			System.out.println("User done adv search");
+		}
+		
+		@AfterClass
+		public void logout() {
+			
+			System.out.println("User logged out");
+			
+		}
+
+	}
+}
