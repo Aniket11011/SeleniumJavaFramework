50d7c7a32667897bb730a94fd894b1ea5c8c3ec1
aniket <aniketsalunkhe1004@gmail.com>
2026-07-28 13:10:53 +0530

    Done Changes in logout method

--- Practice/src/test/java/Practice/Practice.java
+++ Practice/src/test/java/Practice/Practice.java
@@ -44,6 +44,7 @@ public class Practice {
 			System.out.println("User logged out");
 			System.out.println("Welcome message displayed correctly");
 			System.out.println("logo Displayed");
+			System.out.println("logo should not be Displayed");
 			
 		}
 
